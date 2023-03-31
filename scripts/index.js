82f2dfd316a0902f2b8257a57de28ac33417d417
Leo <debonnetleo4@gmail.com>
2023-03-31 20:18:47 +0200

    about

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
     $("#home").on("click", function(){
         $(".home").animate({marginLeft: "0"}, 1000);
+        $(".about").animate({marginTop: "0"}, 1000);
     });
 
     $("#about").on("click", function(){
         $(".home").animate({marginLeft:"-100vw"}, 1000);
+        $(".about").animate({marginTop: "-100vh"}, 1000);
     });
 
     $("#projects").on("click", function(){
         $(".home").animate({marginLeft:"-100vw"}, 1000);
+        $(".about").animate({marginTop: "0"}, 1000);
     });
 
     $("#contact").on("click", function(){
         $(".home").animate({marginLeft:"-100vw"}, 1000);
+        $(".about").animate({marginTop: "0"}, 1000);
     });
 });
