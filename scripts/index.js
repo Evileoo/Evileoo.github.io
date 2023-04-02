1b98fab2b5db096ff932d9ad1b2f76c406944409
Leo <debonnetleo4@gmail.com>
2023-04-02 17:30:57 +0200

    contact

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,34 @@
 $(document).ready(function(){
     $(".projects").css({marginTop:"-300vh"});
+    $(".contact").css({marginTop:"0vh", marginLeft:"100vw"});
 
     $("#home").on("click", function(){
         $(".home").animate({marginLeft: "0"}, 1000);
         $(".about").animate({marginTop: "0"}, 1000);
         $(".projects").animate({marginTop: "-300vh"}, 1000);
+        $(".contact").animate({marginLeft:"100vw"}, 1000);
     });
 
     $("#about").on("click", function(){
         $(".home").animate({marginLeft:"-100vw"}, 1000);
         $(".about").animate({marginTop: "-100vh"}, 1000);
         $(".projects").animate({marginTop: "-300vh"}, 1000);
+        $(".contact").animate({marginLeft:"100vw", marginTop:"100vh"}, 1000);
     });
 
     $("#projects").on("click", function(){
         $(".home").animate({marginLeft:"-100vw"}, 1000);
         $(".about").animate({marginTop: "0"}, 1000);
         $(".projects").animate({marginTop: "-200vh"}, 1000);
+        $(".contact").animate({marginLeft:"100vw", marginTop:"-100vh"}, 1000);
     });
 
     $("#contact").on("click", function(){
         $(".home").animate({marginLeft:"-100vw"}, 1000);
         $(".about").animate({marginTop: "0"}, 1000);
         $(".projects").animate({marginTop: "-300vh"}, 1000);
+        $(".contact").animate({marginLeft: "0", marginTop:"0"}, 1000);
     });
-
-    $(".preview").hover(
-        () => {
-            //console.log($(this).index(this));
-            //$(this).find(".view").css({transform:"scale(1)", height:"auto", width: "auto"});
-        }, 
-        () => {
-            //$(this).find(".view").css({transform:"scale(0)", height:"0", width: "0"});
-        }
-    );
 
     $(".projects > .projectBar > .project").on("click", function(){
         var id = $(this).attr('id');
